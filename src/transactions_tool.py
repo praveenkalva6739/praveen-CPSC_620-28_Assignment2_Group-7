77054451ddd9dc441bf59032e668dbc019231c1a
RakshanaaKumar <RAKSHANAA.KUMAR4191@MYUNFC.CA>
2025-11-22 04:22:24 +0000

    Task 25 : Added monthly net amount plot function

--- src/transactions_tool.py
+++ src/transactions_tool.py
@@ -90,7 +90,7 @@ def generate_overall_financial_summary(df: pd.DataFrame) -> dict:
     avg_amount = df["amount"].mean()
     max_amount = df["amount"].max()
     min_amount = df["amount"].min()
-
+ 
     summary = {
         "total_transactions": total_transactions,
         "total_credit": total_credit,
